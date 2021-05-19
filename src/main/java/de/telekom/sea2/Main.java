aaba301db6a6150ef6398b22a969a1e63ed8d3f7
juliShmi <juliashmitko@gmail.com>
2021-05-19 14:57:55 +0300

    Person, Seminar, Main

--- src/main/java/de/telekom/sea2/Main.java
+++ src/main/java/de/telekom/sea2/Main.java
@@ -1,9 +1,9 @@
 package de.telekom.sea2;
 
-
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+		SeminarApp app = new SeminarApp();
+		app.run(args);
 	}
 }
--- src/main/java/de/telekom/sea2/SeminarApp.java
+++ src/main/java/de/telekom/sea2/SeminarApp.java
@@ -2,4 +2,8 @@ package de.telekom.sea2;
 
 public class SeminarApp {
 
+	public void run(String[] args) {
+
+	}
+
 }
